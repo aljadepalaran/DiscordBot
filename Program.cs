85186b9a8eac563571a63281b56a6a12e91fd156
Al Jade Palaran <aljadepalaran@Als-MacBook-Air.local>
2026-07-09 15:54:19 +0100

    add example commands

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Hosting;
+
+using NetCord;
+using NetCord.Hosting.Gateway;
+using NetCord.Hosting.Services;
+using NetCord.Hosting.Services.ApplicationCommands;
+using NetCord.Rest;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services
+  .AddDiscordGateway(options =>
+    {
+        options.Token = builder.Configuration["Discord:Token"]!;
+    })
+    .AddApplicationCommands();
+
+var host = builder.Build();
+
+
+host.AddSlashCommand("ping", "Ping!", () => "Pong!");
+host.AddUserCommand("Username", (User user) => user.Username);
+host.AddMessageCommand("Length", (RestMessage message) => message.Content.Length.ToString());
+
+// Add commands from modules
+// host.AddModules(typeof(Program).Assembly);
+
+await host.RunAsync();
